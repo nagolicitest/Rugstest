f305b1d15386c61fe7a78dfa0682faf386cfd8f2
Modz <matt@smartmovementmarketing.co.uk>
2021-03-10 23:17:41 +0000

    home

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Navbar />
     
       <Switch>
-        <Route path='/' exact component={Home}/>
+        <Route path='/Rugstest' exact component={Home}/>
         <Route path='/Ship' component={Ship}/>
         <Route path='/Overview' component={Overview}/>
 
